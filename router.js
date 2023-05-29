8745c7eb01f2e8194a58903a46bc04a1ac8d63a6
sardor93-github <akramovsardor060393@gmail.com>
2023-05-29 12:15:36 +0900

    feat: to build Member Controller

--- router.js
+++ router.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController")
 
-router.get("/", function (req, res) {
-    res.send("home sahifasidasiz");
-});
+//memberga oid routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+
+//other router
 router.get("/menu", (req, res) => {
     res.send("menu sahifasidasiz");
 });
--- train.js
+++ train.js
@@ -1,3 +1,4 @@
+// isPrime sonni aniqlash
 console.log("Sonning tub yoki tub emasligini aniqlash!!!");
 
 function tubSon(raqam){
@@ -16,3 +17,27 @@ console.log(tubSon(11));
 console.log(tubSon(25));
 console.log(tubSon(67));
 console.log(tubSon(43));
+
+
+
+// challenge 3 ning code i
+// console.log("Challenge 3ni Boshladik!!!");
+// const pricess = [2, 3, 6, 1, 5, 4, 20, 30];
+// console.log("You should buy stock => ", buyStock(pricess));
+
+
+// function buyStock(stock) {
+//     let buy_qiymat = pricess[0];
+//     let buy_qiymat_max = pricess[0];
+//     for (let i = 0; i <= pricess.length; i++) {
+//         if (buy_qiymat >= pricess[i]) {
+//             buy_qiymat = pricess[i];
+//         }
+//         if (buy_qiymat_max <= pricess[i]) {
+//             buy_qiymat_max = pricess[i];
+//         }
+//     }
+//     let diff = buy_qiymat_max - buy_qiymat;
+//     const index = pricess.indexOf(buy_qiymat);
+//     return  `{ index: ${index}, profit: ${diff} }`;
+// }
