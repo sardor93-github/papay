1b42b34749e263f4732135a6f658cae990f234cd
sardor93-github <akramovsardor060393@gmail.com>
2023-06-21 20:55:04 +0900

    feat: develop new product creation logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -103,7 +103,6 @@ restaurantController.loginProcess = async (req, res) => {
 restaurantController.logout = (req, res) => {
     try {
         console.log("GET cont/logout");
-        console.log("REQ SESSION:", req.session.destroy);
         req.session.destroy(function () {
             res.redirect("/resto");
         });
